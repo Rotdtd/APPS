318090ddc9c0639b7fd80a19138d65a9a8fc3e27
Rotdtd <108656964+Rotdtd@users.noreply.github.com>
2025-12-21 13:49:21 -0500

    Update install_agnudp.sh

--- install_agnudp.sh
+++ install_agnudp.sh
@@ -645,7 +645,7 @@ perform_install_manager_script() {
     local _symlink_path="/usr/local/bin/agnudp"
     
     echo "Downloading manager script..."
-    curl -o "$_manager_script" "https://github.com/khaledagn/AGN-UDP/raw/main/agnudp_manager.sh"
+    curl -o "$_manager_script" "https://raw.githubusercontent.com/Rotdtd/APPS/refs/heads/main/agnudp_manager.sh"
     chmod +x "$_manager_script"
     
     echo "Creating symbolic link to run the manager script using 'agnudp' command..."
